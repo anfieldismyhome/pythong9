de873339926a8ad81c4ecfdd28052d4836e193e8
anfieldismyhome <zubinjcoach@gmail.com>
2026-07-07 03:57:37 +0800

    Create components.js

---
+++ components.js
@@ -0,0 +1,10 @@
+/* ==========================================================
+GRADE 9 PYTHON
+COMPONENT LIBRARY
+========================================================== */
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+console.log("Components Loaded");
+
+});
