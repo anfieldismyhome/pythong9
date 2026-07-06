2b8ed198a17f8e310bf5ddfc162b8a50586c64b7
anfieldismyhome <zubinjcoach@gmail.com>
2026-07-07 03:59:38 +0800

    Implement copy code button functionality
    Added functionality for copy code button to clipboard.

--- components.js
+++ components.js
@@ -8,3 +8,29 @@ document.addEventListener("DOMContentLoaded",()=>{
 console.log("Components Loaded");
 
 });
+/* ==========================================================
+COPY CODE BUTTON
+========================================================== */
+
+document.querySelectorAll(".copy-button").forEach(button=>{
+
+button.addEventListener("click",()=>{
+
+const code=button.closest(".code-card")
+.querySelector("pre").innerText;
+
+navigator.clipboard.writeText(code);
+
+const original=button.innerText;
+
+button.innerText="Copied!";
+
+setTimeout(()=>{
+
+button.innerText=original;
+
+},1500);
+
+});
+
+});
